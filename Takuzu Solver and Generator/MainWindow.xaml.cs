b3d5b9cb51b84cf4d04c03eb20532688c587b87a
Dwight VdV <dfr34kz@gmail.com>
2017-12-15 23:10:37 +0100

    Initial push; Did all the GUI

---
+++ Takuzu Solver and Generator/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Takuzu_Solver_and_Generator {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Gen_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnSave_Gen_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnSolve_Sol_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnCheck_Play_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void TextBox_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+
+        }
+
+        private void btnFill_Play_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnFill_Sol_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
